0a8f47478d365e2352b8e29dac1affce8a312674
JustinBiga <BISENGOJ@GMAIL.COM>
2024-03-21 16:14:37 -0400

    variable complete 2

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿//Declare and initialize variables
+string dogName = "Ralph";
+int dogAge = 10;
+char middleInitial = 'S';
+bool isLess15 = true;
+double currentHeight = 4.85;
+decimal currentWeight = 60.57m;
+
+
+// Writing the variables to the console
+
+Console.WriteLine($"My dog name is {dogName}, He is {dogAge} years old, His midde initial is {middleInitial}, less than 15 years old {isLess15}, current Height {currentHeight}, current weight {currentWeight}");
